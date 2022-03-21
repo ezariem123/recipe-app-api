fd147d0b40921e0b252a7c794fcb8c8c9b9aed0d
Anna Gruszczynska <anna_gruszczynska@outlook.com>
2022-03-21 18:07:38 +0100

    Added create ingridient feature

--- app/recipe/tests/test_ingridients_api.py
+++ app/recipe/tests/test_ingridients_api.py
@@ -65,3 +65,21 @@ class PrivateIngridientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingridient.name)
+
+    def test_create_ingridients_successfull(self):
+        """Test creating new ingridients"""
+        payload = {'name': 'Cherry'}
+        self.client.post(INGRIDEINTS_URL, payload)
+
+        exists = Ingridient.objects.filter(
+            user=self.user,
+            name=payload['name'],
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_ingridients_invalid(self):
+        """Test creating ingridient with invalid payload"""
+        payload = {'name': ''}
+        res = self.client.post(INGRIDEINTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
